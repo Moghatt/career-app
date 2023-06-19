d7158ba8fc1c644070f0a9d7466366b5e3c456d5
moghatt <mohammad_ghatali_kh@yahoo.com>
2023-06-19 14:57:08 +0930

    first commit

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+
+const Landing = () => {
+  return (
+    <div>
+      Landing Page
+    </div>
+  )
+}
+
+export default Landing
+
+
